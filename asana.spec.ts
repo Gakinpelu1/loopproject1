21a9ccf233d2540c66f8c6f66193f59a891df73c
Gordon Akinpelu <gordon.j.akinpelu@gmail.com>
2024-03-10 20:20:27 -0500

    refactoring

--- asana.spec.ts
+++ asana.spec.ts
@@ -1,32 +1,24 @@
 import { test, expect } from '@playwright/test';
 const testCases = JSON.parse(JSON.stringify(require("./testinfo.json")))
-const loginInfo = JSON.parse(JSON.stringify(require("./login.json")))
-const Trouble = require("./troubleshoot")
+import { Toolbox } from '../tests-examples/toolbox'; //contains the log in method
 
 
 
 testCases.forEach(data => {
 
-    test(`run tests when logged into Asana ${data.name}`, async ({page}) => {
-        
-        //navigate to page
-        await page.goto(loginInfo.loginPage)
-        //enter in email address
-        await page.fill("input",loginInfo.email)
-        //click continue
-        await page.getByRole('button', { name: 'Continue', exact: true }).click()
-        await page.waitForTimeout(2000)
-        //enter in password
-        await page.fill("#lui_6", loginInfo.password)
-        //click Log in
-        await page.getByRole('button', { name: 'Log in', exact: true }).click()
-
-        //Navigate to
-        //page. click on span that has text _______
+    test.only(`run tests when logged into Asana ${data.name}`, async ({page}) => {
+    
+        const toolbox = new Toolbox(page)
+        await toolbox.loggingIn();
+        //once logged in, click on a left hand side navigation menu link:
+        await page.getByRole("link", {name:data.leftNav}).first().click()
+        await page.waitForTimeout(4000)
+        //Navigate to sidebar nav option and click on span that has text _______
         await page.getByRole("link", {name:data.leftNav}).click()
         await page.waitForTimeout(3000)
         //expect
         //await expect(page.getByText(${data.card_title})
+        await expect(page.locator('.BoardCard-taskName')).toContainText(data.card_title)
 
     })
 
