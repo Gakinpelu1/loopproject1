92ae43907af2b13deca6dc6bd0d9a1b5665756a0
Gordon Akinpelu <gordon.j.akinpelu@gmail.com>
2024-03-13 16:54:18 -0500

    addingstuff

--- asana.spec.ts
+++ asana.spec.ts
@@ -2,23 +2,39 @@ import { test, expect } from '@playwright/test';
 const testCases = JSON.parse(JSON.stringify(require("./testinfo.json")))
 import { Toolbox } from '../tests-examples/toolbox'; //contains the log in method
 
-
-
 testCases.forEach(data => {
 
     test.only(`run tests when logged into Asana ${data.name}`, async ({page}) => {
-    
+        
         const toolbox = new Toolbox(page)
         await toolbox.loggingIn();
         //once logged in, click on a left hand side navigation menu link:
         await page.getByRole("link", {name:data.leftNav}).first().click()
-        await page.waitForTimeout(4000)
-        //Navigate to sidebar nav option and click on span that has text _______
-        await page.getByRole("link", {name:data.leftNav}).click()
+        //await page.getByRole("link", {name:data.leftNav}).click()
         await page.waitForTimeout(3000)
-        //expect
-        //await expect(page.getByText(${data.card_title})
-        await expect(page.locator('.BoardCard-taskName')).toContainText(data.card_title)
+
+        //card title text variable
+        const cardTitle= await page.locator('span', {hasText:`${data.card_title}`}).first()
+      
+        //Assert that Card Titles unique to each test case are visible
+        if(data.id===1){
+            await expect(cardTitle).toBeVisible()
+            }
+            else if (data.id===2) {
+                await expect(cardTitle).toBeVisible()
+            }
+            else if (data.id===3) {
+                await expect(cardTitle).toBeVisible()  
+            }
+            else if (data.id===4) {
+                expect(cardTitle).toBeVisible()
+            } 
+            else if (data.id===5) {
+                expect(cardTitle).toBeVisible()
+            }          
+            else if (data.id===6) {
+                expect(cardTitle).toBeVisible() 
+            }
 
     })
 
