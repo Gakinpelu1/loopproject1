bb23c27802c4bdf10f940c28d7c2486b1ab70782
Gordon Akinpelu <gordon.j.akinpelu@gmail.com>
2024-03-19 21:05:49 -0500

    revisions

--- asana.spec.ts
+++ asana.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 const testCases = JSON.parse(JSON.stringify(require("./testinfo.json")))
 import { Toolbox } from './toolbox'; //contains the log in method
 
+
 testCases.forEach(data => {
 
     test.only(`run tests when logged into Asana ${data.name}`, async ({page}) => {
@@ -13,29 +14,81 @@ testCases.forEach(data => {
         //await page.getByRole("link", {name:data.leftNav}).click()
         await page.waitForTimeout(3000)
 
-        //card title text variable
-        const cardTitle= await page.locator('span', {hasText:`${data.card_title}`}).first()
-      
+        /************************************************************************* 
+
+        The below is what I had in mind to loop through the tags; still was having issues trying to isolate
+        the tag's tooltip text...
+
+            const expectedTagsArray = data.tags
+            //locator that will capture all of the tags in a certain 'box'
+            const visibleTagsArray = page.$$('BoardCardCustomPropertiesAndTags-tag')  <-(??)
+
+            function checkTags(expectedTags, visibleTagsArray) {
+                let visibleTags = [];
+                for(let i = 0; i< expectedTags.length; i++){
+                    if(visibleTagsArray.indexOf(expectedTags[i] ! == -1) {
+                        visibleTags.push(expectedTags[i]);
+                    }
+                }
+                return visibleTags;
+                //Expect the new array of matched tags to equal the tags noted in each Test Case
+                await expect(visibleTags).toContainEqual(expectedTags)
+            } 
+            
+            *******************************************************************/
+        
+    
         //Assert that Card Titles unique to each test case are visible
         if(data.id===1){
-            await expect(cardTitle).toBeVisible()
+            const toDoColumnCardTitle = page.locator('.BoardBody-columnDraggableItemWrapper.SortableList-sortableItemContainer').filter(
+                {hasText: `${data.card_title}`})
+                await expect(toDoColumnCardTitle).toHaveCount(1)
+                /*Assert tags
+                checkTags(expectedTags, visibleTagsArray)
+                */   
+
             }
             else if (data.id===2) {
-                await expect(cardTitle).toBeVisible()
+                const toDoColumnCardTitle = page.locator('.BoardBody-columnDraggableItemWrapper.SortableList-sortableItemContainer').filter(
+                {hasText: `${data.card_title}`})
+                await expect(toDoColumnCardTitle).toHaveCount(1)
+                /*Assert tags
+                checkTags(expectedTags, visibleTagsArray)
+                */   
             }
             else if (data.id===3) {
-                await expect(cardTitle).toBeVisible()  
+                const toDoColumnCardTitle = page.locator('.BoardBody-columnDraggableItemWrapper.SortableList-sortableItemContainer').filter(
+                {hasText: `${data.card_title}`})
+                await expect(toDoColumnCardTitle).toHaveCount(1) 
+                 /*Assert tags
+                checkTags(expectedTags, visibleTagsArray)
+                */   
             }
             else if (data.id===4) {
-                expect(cardTitle).toBeVisible()
-            } 
+                const newRequestColumnCardTitle = page.locator('.BoardBody-columnDraggableItemWrapper.SortableList-sortableItemContainer').first().filter(
+                {hasText: `${data.card_title}`})
+                await expect(newRequestColumnCardTitle).toHaveCount(1)
+                 /*Assert tags
+                checkTags(expectedTags, visibleTagsArray)
+                */   
+            }
             else if (data.id===5) {
-                expect(cardTitle).toBeVisible()
+                const inProgressColumnCardTitle = page.locator('.BoardBody-columnDraggableItemWrapper.SortableList-sortableItemContainer').nth(2).filter(
+                {hasText: `${data.card_title}`})    
+                await expect(inProgressColumnCardTitle).toHaveCount(1)
+                 /*Assert tags
+                checkTags(expectedTags, visibleTagsArray)
+                */   
             }          
             else if (data.id===6) {
-                expect(cardTitle).toBeVisible() 
+                const completeColumnCardTitle = page.locator('.BoardBody-columnDraggableItemWrapper.SortableList-sortableItemContainer').nth(3).filter(
+                {hasText: `${data.card_title}`})
+                await expect(completeColumnCardTitle).toHaveCount(1) 
+                 /*Assert tags
+                checkTags(expectedTags, visibleTagsArray)
+                */     
             }
-
+        
     })
 
 }) 
